7b61d9b8be1adfc4eaba2bf47e9ef4b7cf06c74b
Anirudh V <anirudhvemula123@gmail.com>
2025-06-01 19:05:53 +0000

    i cannot give my storage bucket , do without that

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,15 +4,26 @@ import { getAuth } from 'firebase/auth';
 // import { getFirestore } from 'firebase/firestore';
 // import { getStorage } from 'firebase/storage';
 
-const firebaseConfig = {
+const firebaseConfig: {
+  apiKey?: string;
+  authDomain?: string;
+  projectId?: string;
+  storageBucket?: string;
+  messagingSenderId?: string;
+  appId?: string;
+  measurementId?: string;
+} = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
+
+if (process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET) {
+  firebaseConfig.storageBucket = process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET;
+}
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
@@ -21,3 +32,4 @@ const auth = getAuth(app);
 // const storage = getStorage(app);
 
 export { app, auth /*, db, storage */ };
+
