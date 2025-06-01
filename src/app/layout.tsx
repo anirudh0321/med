5b2008a9d902e6aa31543259f5acbf994e075c24
Anirudh V <anirudhvemula123@gmail.com>
2025-06-01 20:25:59 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > Console Error: In HTML, whitespace text nodes cannot be a child of <html>. Make sure you don't have any extra whitespace between tags on each line of your source code.
    > This will cause a hydration error.. Error source: See more info here: https://nextjs.org/docs/messages/react-hydration-error
    >
    > ...
    >     <HotReload assetPrefix="" globalError={[...]}>
    >       <AppDevOverlay state={{nextId:1, ...}} globalError={[...]}>
    >         <AppDevOverlayErrorBoundary globalError={[...]} onError={function bound dispatchSetState}>
    >           <ReplaySsrOnlyErrors>
    >           <DevRootHTTPAccessFallbackBoundary>
    >             <HTTPAccessFallbackBoundary notFound={<NotAllowedRootHTTPFallbackError>}>
    >               <HTTPAccessFallbackErrorBoundary pathname="/dashboard" notFound={<NotAllowedRootHTTPFallbackError>} ...>
    >                 <RedirectBoundary>
    >                   <RedirectErrorBoundary router={{...}}>
    >                     <Head>
    >                     <link>
    >                     <script>
    >                     <script>
    >                     <script>
    >                     <RootLayout>
    > >                     <html lang="en" className="dark">
    > >                       {" "}
    >                         ...
    >                     ...
    >         ...
    >
    >
    > src/app/layout.tsx (17:5) @ RootLayout
    >
    >   15 | }>) {
    >   16 |   return (
    > > 17 |     <html lang="en" className="dark"> {/* Added 'dark' class here */}
    >      |     ^
    >   18 |       <head>
    >   19 |         <link rel="preconnect" href="https://fonts.googleapis.com" />
    >   20 |         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
    >
    > Call Stack
    > 14
    >
    > Show 12 ignore-listed frame(s)
    > html
    > <anonymous> (0:0)
    > RootLayout
    > src/app/layout.tsx (17:5)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark"> {/* Added 'dark' class here */}
+    <html lang="en" className="dark">{/* Added 'dark' class here */}
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
